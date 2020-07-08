5073e93c26358005e36647b3a2c07a2075c49549
Jack Marquez <jackdmarquez@gmail.com>
2020-07-08 11:30:29 -0500

    using argparse to parse args

--- pi.py
+++ pi.py
@@ -3,6 +3,12 @@ from random import random as r
 from math import pow as p
 from sys import argv
 
+
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("number", help="number of random points (int)", type=int)
+args = parser.parse_args()
+
 # Make sure number of attempts is given on command line
 assert len(argv) == 2
 attempts = int(argv[1])
